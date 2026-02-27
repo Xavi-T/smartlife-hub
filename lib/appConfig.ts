987b4d267be11f56980d7b6ce391327b5cde0250
SonHip <sonhipkmhd@gmail.com>
2026-02-27 15:45:05 +0700

    fix: update shop phone number and Zalo link in app configuration for accuracy

--- lib/appConfig.ts
+++ lib/appConfig.ts
@@ -2,7 +2,7 @@ export const APP_CONFIG = {
   shopName: "SmartLife Hub",
   shopTagline: "Mang sự tiện lợi đến với mọi gia đình Việt",
   shopAddress: "142 Đ. Tô Hiến Thành, P. Nguyễn, Đại Năng, TP. Hải Phòng",
-  shopPhone: "0968 390 201",
+  shopPhone: "0838 709 126",
   taxCode: "0108390511",
   shopEmail: "smartlifehub.work@gmail.com",
   shopWebsite: "https://smartlife-hub.shop",
@@ -12,7 +12,7 @@ export const APP_CONFIG = {
     accountName: "CONG TY SMARTLIFE HUB",
   },
   socials: {
-    zalo: "https://zalo.me/0968390201",
+    zalo: "https://zalo.me/0838709126",
     messenger: "https://www.facebook.com/profile.php?id=61587876603153",
     facebook: "https://facebook.com/61587876603153",
     youtube: "https://www.youtube.com/channel/ucw0Ig-sxjw0wbcsd4hi8n3g",
